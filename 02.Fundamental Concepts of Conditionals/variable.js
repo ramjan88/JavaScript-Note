6cd30e425a763ad3859bc63c3c0e8c464543ece2
ramjan88 <gmramjanali8888@gmail.com>
2025-02-05 23:59:01 +0600

    added more js

---
+++ 02.Fundamental Concepts of Conditionals/variable.js
@@ -0,0 +1,37 @@
+/**
+ * VARIABLE ==> Container -- many kind of value accumulate;
+ */
+
+// number
+var price = 30;
+
+
+//string
+var name = 'Ramjan ali';
+
+
+//boolean
+var passed = true;
+
+
+/**
+ *  NAMING CONVENTION
+ * 1. single word
+ * 2. no quate
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be use reserve keyword
+ * 6. prefer to camelCase 
+ */
+
+var lastExamResult = 90;
+
+/**
+ * OPARATION
+ * 1. +, -, *, /, % 
+ * 2. +=, -=, *=, /=
+ * 3. isNaN?
+ * 4. null
+ * 5. Number
+ * 
+ */
